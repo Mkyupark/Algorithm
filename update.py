c3617191f1cee9efb3dfc11c23eb94d028b5fd2b
Mkyupark <102354411+Mkyupark@users.noreply.github.com>
2023-12-28 16:32:46 +0900

    Update update.py

--- update.py
+++ update.py
@@ -7,9 +7,6 @@ HEADER="""#
 # 백준 & 프로그래머스 문제 풀이 목록
 [백준](https://solved.ac/profile/m_kyu)
 [프로필](https://career.programmers.co.kr/pr/innerpeace0828_8619)
-
-프로그래머스의 경우, 푼 문제 목록에 대한 마이그레이션이 필요합니다.
-
 """
 
 def main():
