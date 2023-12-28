f21e8ebfc17e67e0ca2b31cde1bc8e1313e07d10
Mkyupark <102354411+Mkyupark@users.noreply.github.com>
2023-12-28 11:20:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,49 +1,44 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
-int bfs(int me, int brother) {
-    vector<int> visited(100001, -1);  // 방문 여부 및 거리를 저장하는 배열
+int bfs(int me, int brother){
+    vector<int> visited(100001,-1);
     queue<int> q;
-
-    visited[me] = 0;
+    
+    visited[me] =0;
     q.push(me);
-
-    while (!q.empty()) {
+    while(!q.empty()){
         int cur = q.front();
         q.pop();
-
-        if (cur == brother) {
+        
+        if(cur ==brother){
             return visited[cur];
         }
-
-        if (cur - 1 >= 0 && visited[cur - 1] == -1) {
-            visited[cur - 1] = visited[cur] + 1;
-            q.push(cur - 1);
+        
+        if(cur-1>=0 && visited[cur-1] == -1){
+            visited[cur-1] = visited[cur] +1;
+            q.push(cur-1);
         }
-
-        if (cur + 1 <= 100000 && visited[cur + 1] == -1) {
-            visited[cur + 1] = visited[cur] + 1;
-            q.push(cur + 1);
+        
+        if(cur +1 <=100000 &&visited[cur+1] == -1){
+            visited[cur+1] = visited[cur] +1;
+            q.push(cur+1);
         }
-
-        if (2 * cur <= 100000 && visited[2 * cur] == -1) {
-            visited[2 * cur] = visited[cur] + 1;
-            q.push(2 * cur);
+        
+        if(cur*2 <=100000 && visited[cur*2] == -1){
+            visited[cur*2] = visited[cur] +1;
+            q.push(cur*2);
         }
     }
-
-    return -1;  // 찾을 수 없는 경우
+    return -1;
 }
 
-int main() {
+int main(){
     int me, brother;
-    cin >> me >> brother;
-
-    int result = bfs(me, brother);
-    
-    cout << result << endl;
-
-    return 0;
+    cin >>me >>brother;
+    int answer = bfs(me, brother);
+    cout << answer;
 }
