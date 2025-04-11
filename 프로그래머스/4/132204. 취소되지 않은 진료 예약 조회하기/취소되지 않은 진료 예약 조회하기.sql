888851dd9a72e44eae5f6e9c550d2710f9638e8a
Mkyupark <102354411+Mkyupark@users.noreply.github.com>
2025-04-11 14:46:49 +0900

    Update 취소되지 않은 진료 예약 조회하기.sql

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -10,9 +10,26 @@ SELECT
     ON P.PT_NO = A.PT_NO
   JOIN DOCTOR D
     ON D.DR_ID = A.MDDR_ID
- WHERE 1=1
-   AND TO_CHAR(APNT_YMD,'YYYYMMDD') = '20220413'
+ WHERE TO_CHAR(APNT_YMD,'YYYYMMDD') = '20220413'
    AND NVL(A.APNT_CNCL_YN,'N') != 'Y'
    AND A.MCDP_CD = 'CS'
  ORDER
     BY A.APNT_YMD ASC
+/
+-- 채점 기준에서 ANSI JOIN을 쓰지 않으면 안되는 것 같음
+-- 아래는 틀렸다고 나옴
+SELECT a.APNT_NO
+      ,p.PT_NAME
+      ,a.PT_NO
+      ,a.MCDP_CD
+      ,d.DR_NAME
+      ,a.APNT_YMD
+FROM   APPOINTMENT a
+      ,patient     p
+      ,DOCTOR      d
+WHERE  a.PT_NO = p.PT_NO
+       AND a.MDDR_ID = d.DR_ID
+       AND a.MCDP_CD = 'CS'
+       AND a.APNT_CNCL_YN = 'N'
+       AND TO_CHAR(a.APNT_YMD, 'YYYYMMDD') = '20220413'
+ORDER  BY a.APNT_NO;
